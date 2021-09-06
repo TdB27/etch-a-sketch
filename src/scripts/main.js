8e433b50bcf17f8e9fa00b1c9601e18a7976f675
Thiago Billy <thiagobilly27@gmail.com>
2021-09-06 16:57:26 -0300

    ajustes no input range e organização dos buttons

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,23 @@
-let input = document.querySelector('.navigation__frame-size-input')
+const buttonDefault = document.querySelector('.navigation__default')
+const buttonRandom = document.querySelector('.navigation__colors-random');
+const buttonErase = document.querySelector('.navigation__erase');
+const buttonClear = document.querySelector('.navigation__clear');
+const buttonGrid = document.querySelector('.navigation__grid');
+let changeParagraph = document.querySelector('.navigation__frame-size-box__paragraph');
+let input = document.querySelector('.navigation__frame-size-input');
 
-input.addEventListener('mousemove', () => {
-    let changeParagraph = document.querySelector('.navigation__frame-size-box__paragraph')
-    changeParagraph.innerHTML = `${input.value} x ${input.value}`
-})
+buttonDefault.onclick = () => changeColor('default')
+buttonRandom.onclick = () => changeColor('random');
+buttonErase.onclick = () => changeColor('erase');
+buttonClear.onclick = () => changeColor('clear');
+buttonGrid.onclick = () => changeColor('grid');
+input.onmousemove = (e) => changeParagraphforInput(e.target.value)
 
+
+
+function changeParagraphforInput(value) {
+    changeParagraph.innerHTML = `${value} x ${value}`
+}
 
 function changeColor(value) {
     let changeColorButton = document.querySelectorAll('[data-color]');
