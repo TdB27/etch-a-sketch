ea671d3493770d13e67e64c8b7f6cfe216ad2a52
Thiago Billy <thiagobilly27@gmail.com>
2021-09-06 16:33:56 -0300

    mudar o p conforme o input

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
+let input = document.querySelector('.navigation__frame-size-input')
+
+input.addEventListener('mousemove', () => {
+    let changeParagraph = document.querySelector('.navigation__frame-size-box__paragraph')
+    changeParagraph.innerHTML = `${input.value} x ${input.value}`
+})
+
+
 function changeColor(value) {
     let changeColorButton = document.querySelectorAll('[data-color]');
     
